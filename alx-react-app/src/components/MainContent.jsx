a73475b09f2ac7379a04acfffa90e4a9778d6a87
anthonygift <omonagoanthony@gmail.com>
2025-08-01 14:44:42 -0700

    'maincontenet

---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main>
+      <h2>Welcome to the Main Content</h2>
+       <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
