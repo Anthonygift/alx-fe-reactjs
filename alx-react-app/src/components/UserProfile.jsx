b2eda0142830f5e79dd669fdedac4295a32deb29
anthonygift <omonagoanthony@gmail.com>
2025-08-01 15:23:07 -0700

    index

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
-function UserProfile(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>{props.age}</p>
-      <p> {props.bio}</p>
-    </div>
-  );
-}
- export default UserProfile;s
+const UserProfile = (props) => {
+   return (
+     <div>
+       <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+     </div>
+   );
+ };
+export default UserProfile;
