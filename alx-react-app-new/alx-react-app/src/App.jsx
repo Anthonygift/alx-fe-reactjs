b2b9d2a9724108ccbcede940f3d9fd8e6ad7772f
anthonygift <omonagoanthony@gmail.com>
2025-08-12 04:28:35 -0700

    fli

--- alx-react-app-new/alx-react-app/src/App.jsx
+++ alx-react-app-new/alx-react-app/src/App.jsx
@@ -42,6 +42,21 @@ function App() {
     </Router>
   );
 }
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
 
 
 import WelcomeMessage from './components/WelcomeMessage'
