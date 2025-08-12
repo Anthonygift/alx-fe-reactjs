b2f101f9edb5461761e55c302d72c20a738857fb
anthonygift <omonagoanthony@gmail.com>
2025-08-12 02:37:22 -0700

    index

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/Counter';
+function App() {
+  return (
+    <div>
+      <h1>My React App with Props</h1>
+      <Counter />
+    </div>
+  );
+}
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
