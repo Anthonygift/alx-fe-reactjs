6c302fc86a781a6666677f77a3d6fe0aa1571920
anthonygift <omonagoanthony@gmail.com>
2025-08-12 01:40:10 -0700

    file

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
   return (
-    <main>
+    <main style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }} >
       <h2>Welcome to the Main Content</h2>
        <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
