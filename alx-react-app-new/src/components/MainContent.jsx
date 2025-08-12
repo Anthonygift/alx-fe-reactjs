87f9af0d95accfb7a8536641f61c05f0e3b4b02b
anthonygift <omonagoanthony@gmail.com>
2025-08-12 01:59:45 -0700

    day

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }} >
+      <h2>Welcome to the Main Content</h2>
+       <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
