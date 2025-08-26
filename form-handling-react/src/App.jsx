12e4a5a3d9d680530a45b655a0ad4511ba8335d6
anthonygift <omonagoanthony@gmail.com>
2025-08-26 12:27:06 -0700

    word

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from "./components/RegistrationForm"; // controlled form
-import FormikForm from "./components/formikForm"; // formik form
+import FormikForm from "./components/FormikForm"; // formik form
 
 function App() {
   return (
